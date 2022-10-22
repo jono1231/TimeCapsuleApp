582449702bf54d3cd8886be21aa3cc63396ac065
saejune04 <99034245+saejune04@users.noreply.github.com>
2022-10-22 16:39:06 -0700

    repushing sht cuz idk how to use github ffs

--- frontend/src/PostCreationPage.js
+++ frontend/src/PostCreationPage.js
@@ -1,29 +1,57 @@
+import './PostCreationPage.css';
 import React, {useState} from 'react';
 
 export default function PostCreationPage() {
 
     const [postFormData, setPostFormData] = useState(
         {
-            img: "",
-            message: ""
+            message: "",
+            img: {
+                name: "",
+                data: ""
+            }
         }
     );
 
     function handleChange(event) {
-        const {name, value, type, checked} = event.target
+        const {name, value, type, checked} = event.target;
         setPostFormData(prevPostFormData => {
             return {
                 ...prevPostFormData,
-                [name]: type === "checkbox" ? checked : value
+                [name]: type === "checked" ? checked : value
             }
         })
     }
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        // submitToApi(formData)
-        console.log(postFormData)
+
+    function handleFileChange(event) {
+        const {name, value} = event.target;
+        setPostFormData(prevPostFormData => {
+            return {
+                ...prevPostFormData,
+                [name]: {
+                    name: value.name,
+                    data: value
+                }
+            }
+        })
     }
+    //need a validate form function
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setPostFormData(
+            {
+                message: "",
+                img: {
+                    name: "",
+                    data: ""
+                }
+            }
+        )
+        // submitToApi(formData)
+        console.log(postFormData);
+    }
+
 
     return (
         <form className = "postCreationForm" onSubmit = {handleSubmit}>
@@ -33,14 +61,21 @@ export default function PostCreationPage() {
                 value = {postFormData.message}
                 onChange = {handleChange}
                 name = "message"
-            />
-            <input 
-                type = "text"
-                placeholder = "Upload an img"
-                value = {postFormData.img}
-                onChange = {handleChange}
+            /><br />
+            <input className = "postCreationForm--fileinput"
+                type = "file" 
+                accept = "image/jpeg, image/png, image/jpg"
+                value = {postFormData.img.data}
+                onChange = {handleFileChange}
                 name = "img"
             />
+            {/*
+            <button onClick={this.onFileUpload}>
+            Upload!
+            </button>
+            {this.fileData()}
+            <br />
+            */}
             <button>Post</button>
 
         </form>
