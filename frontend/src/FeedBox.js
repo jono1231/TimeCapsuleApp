41dd583f8965d19d15b51566ab7b392c0163effc
vpritamani <44382864+vpritamani@users.noreply.github.com>
2022-10-22 15:47:12 -0700

    Few more style edits

--- frontend/src/FeedBox.js
+++ frontend/src/FeedBox.js
@@ -5,11 +5,11 @@ export default function FeedBox(props) {
     return (
         <div className="feedbox">
             <img className="feedbox--profileimg" src={props.profilePic}></img>
-            <p1 className="feedbox--username">{props.username}</p1><br />
-            <p1 className="feedbox--postdate">Posted on {props.postDate}</p1> <br />
-            <p1 className="feedbox--memorydate">Memory created on {props.memoryDate}</p1><br />
+            <text className="feedbox--username">{props.username}</text> <br/ >
+            <text className="feedbox--postdate">Posted on {props.postDate}</text> 
+            <text className="feedbox--memorydate">Memory created on {props.memoryDate}</text>    
             <img className="feedbox--postimg" src={props.imagePic}></img>{/*maybe add functionality to add multiple images, (extra component for post style) extra formating within the post iself, etc*/}<br />
-            <p1 className="feedbox--note">{props.note}</p1><br />
+            <p className="feedbox--note">{props.note}</p>
         </div>
     );
 }
