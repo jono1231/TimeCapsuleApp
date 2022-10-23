8afedd8bd40decafc1ae928a392328779a65db2b
jono1231 <jonwang2015@gmail.com>
2022-10-22 21:44:53 -0700

    Got mongodb insertion done pog

--- frontend/src/components/PostCreationPage.js
+++ frontend/src/components/PostCreationPage.js
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { createPost } from '../actions/actions.js';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 
 export default function PostCreationPage() {
 
     const [postFormData, setPostFormData] = useState(
         {
             img: "",
-            message: ""
+            message: "",
+            username: ""
         }
     );
-    const dispatch = useDispatch();
 
     function handleChange(event) {
         const {name, value, type, checked} = event.target
@@ -23,10 +21,19 @@ export default function PostCreationPage() {
     }
     
     function handleSubmit(event) {
-        event.preventDefault();
-        dispatch(createPost(postFormData));
-        clear();
-        console.log(postFormData);
+        const postURL = "http://localhost:5000/post" //Our previously set up route in the backend
+        fetch(postURL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ // We should keep the fields consistent for managing this data later
+                creator: postFormData.username,
+                title: postFormData.message,
+                message: postFormData.img,
+            })
+        })
     }
 
     function clear(){
@@ -35,6 +42,13 @@ export default function PostCreationPage() {
 
     return (
         <form className = "postCreationForm" onSubmit = {handleSubmit}>
+            <input 
+                type = "text"
+                placeholder = "Username"
+                value = {postFormData.username}
+                onChange = {handleChange}
+                name = "username"
+            />
             <input 
                 type = "text"
                 placeholder = "Message"
